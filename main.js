bb6eb925822df763cde531a46baef2bc9ecb7468
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-08-22 23:49:44 +0200

    Created gallery with fancy

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import Masonry from 'masonry-layout';
+const buttonToggle = document.querySelector('#toggle-button');
+const navHider = document.querySelector('#nav-tablet');
+const navElements = document.querySelectorAll('.header__link_tablet');
 
-const buttonToggle = document.querySelector('#toggle-button')
+Fancybox.bind('[data-fancybox="gallery"]', {
+});
+navHider.addEventListener('click', function onClickHide() {
+    navElements.forEach(element => {
+        console.log(element.style.display);
+        if (element.style.display === 'none' || element.style.display === '') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none';
+        }
+    });
+});
 buttonToggle.addEventListener('click', function onClick() {
     const realizationContent = document.getElementById('gradient');
     let status = realizationContent.style.visibility;
