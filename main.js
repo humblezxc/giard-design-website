4d826990ed86979e5269e98337e78a2414e57d96
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-08-22 15:29:16 +0200

    Create link to Heroku

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-import Masonry from 'masonry-layout';
-
-const buttonToggle = document.querySelector('#toggle-button')
-buttonToggle.addEventListener('click', function onClick() {
-    const realizationContent = document.getElementById('gradient');
-    let status = realizationContent.style.visibility;
-
-    if (status === 'hidden') {
-        buttonToggle.textContent = 'Rozwiń';
-        realizationContent.style.visibility = 'visible';
-    } else {
-        buttonToggle.textContent = 'Zwiń';
-        realizationContent.style.visibility = 'hidden';
-    }
-});
-
-window.onload = () => {
-    const grid = document.querySelector('.grid');
-
-    const masonry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        gutter: '.gutter-sizer',
-        percentPosition: true
-    });
-};
+// import Masonry from 'masonry-layout';
+//
+// const buttonToggle = document.querySelector('#toggle-button')
+// buttonToggle.addEventListener('click', function onClick() {
+//     const realizationContent = document.getElementById('gradient');
+//     let status = realizationContent.style.visibility;
+//
+//     if (status === 'hidden') {
+//         buttonToggle.textContent = 'Rozwiń';
+//         realizationContent.style.visibility = 'visible';
+//     } else {
+//         buttonToggle.textContent = 'Zwiń';
+//         realizationContent.style.visibility = 'hidden';
+//     }
+// });
+//
+// window.onload = () => {
+//     const grid = document.querySelector('.grid');
+//
+//     const masonry = new Masonry(grid, {
+//         itemSelector: '.grid-item',
+//         gutter: '.gutter-sizer',
+//         percentPosition: true
+//     });
+// };
